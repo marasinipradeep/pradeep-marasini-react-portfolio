33f14c98d8d1c39a6068b8c31ad2b685328bf7d9
marasinipradeep <marasinipradeep@gmail.com>
2020-09-27 09:11:44 +0930

    #Adding material UI icons on Navbar

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {Home,WorkOutlineOutlined,Assignment,PermContactCalendar,Face} from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+  
 }));
 
 export default function Navbar() {
@@ -26,14 +20,11 @@ export default function Navbar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton color="inherit"><Face />About</IconButton>
+          <IconButton color="inherit"><Assignment/> Resume</IconButton>
+          <IconButton color="inherit"><WorkOutlineOutlined/>Works</IconButton>
         </Toolbar>
+        
       </AppBar>
     </div>
   );
