7c1e6dad1b2b4cff9d4a183fe76fdc2d13ef1693
marasinipradeep <marasinipradeep@gmail.com>
2020-09-27 19:31:37 +0930

    #added 1 more project on portfolio

--- portfolio/src/Components/Cards/Cards.js
+++ portfolio/src/Components/Cards/Cards.js
@@ -59,76 +59,71 @@ export default function Cards() {
     };
 
     return (
-
-    
         <>
-            <h1>all Projects</h1>
             {state.length ? (
                 <Card className={classes.root}>
-                    {state.map((project,i) => (
-                        
+                    {state.map((project, i) => (
                         <div>
-                          
-                            
-      <CardHeader
-        avatar={<Avatar aria-label="recipe" className={classes.avatar}>R</Avatar>}
-        action={<IconButton aria-label="settings"><MoreVertIcon /></IconButton>}
-        title={project.fields.projectName}
-        subheader={project.fields.date}
-      />
+                            <CardHeader
+                                avatar={<Avatar aria-label="recipe" className={classes.avatar}>R</Avatar>}
+                                action={<IconButton aria-label="settings"><MoreVertIcon /></IconButton>}
+                                title={project.fields.projectName}
+                                subheader={project.fields.date}
+                            />
 
-      <CardMedia
-        className={classes.media}
-        image={project.fields.images[i].fields.file.url}
-        title="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-         {project.fields.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Technologies Used:</Typography>
-          {project.fields.technoligiesUsed.map(technoligiesUsed=>(
-              <div>
-          <Typography paragraph>
-           {technoligiesUsed}
-          </Typography>
-          </div>
-          
-        ))}
-        
-          
-          
-        </CardContent>
-        
-      </Collapse>
+                            {project.fields.images.map(projectImages => (
+                                <div>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image={projectImages.fields.file.url}
+                                        title="Paella dish"
+                                    />
+                                </div>
+                            ))}
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {project.fields.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton>
+                                <IconButton
+                                    className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                    })}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>Technologies Used:</Typography>
+                                    {project.fields.technoligiesUsed.map(technoligiesUsed => (
+                                        <div>
+                                            <Typography paragraph>
+                                                {technoligiesUsed}
+                                            </Typography>
+                                        </div>
 
-      </div>
-                    ))} 
+                                    ))}
+
+                                </CardContent>
+
+                            </Collapse>
+
+                        </div>
+                    ))}
                 </Card>
-            ) : 
-         
-            (
+            ) :
+                (
                     <h3>No projects Found</h3>
 
                 )}
--- portfolio/src/Components/Utils/API.js
+++ portfolio/src/Components/Utils/API.js
@@ -9,6 +9,39 @@ export default [
         fields: {
 
             date:"September 15 2020",
+            projectName: "Portfolio",
+            description:
+                "This is Simple portfolio which is developed using React,React router, React Hooks and Material UI",
+            githubLink: "https://github.com/marasinipradeep/pradeep-marasini-react-portfolio",
+            liveLink: "https://binout.herokuapp.com/",
+            demoLink: "https://www.youtube.com/watch?v=KxiwXnkHCB8&feature=youtu.be",
+
+            technoligiesUsed: [
+                "React",
+                "React Hooks",
+                "context API",
+                "Reducer",
+                "Material UI"
+            ],
+            images: [
+                {
+                    fields: {
+                        file: {
+                            url: coffeMaker
+                        }
+                    }
+                },
+            ]
+        }
+    },
+
+    {
+        sys: {
+            id: "1"
+        },
+        fields: {
+
+            date:"September 15 2020",
             projectName: "BIN OUT",
             description:
                 "Bin-out is an application which allows users to submit a request for their bins to be taken out for collection when they are unable to.",
@@ -35,4 +68,5 @@ export default [
     },
 
 
+    
 ];
