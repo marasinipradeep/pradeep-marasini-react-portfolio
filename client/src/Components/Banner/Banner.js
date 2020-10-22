9d1c1abbbe00fe8c14b12e365f11eaf9dfc10649
marasinipradeep <marasinipradeep@gmail.com>
2020-10-22 17:51:43 +1030

    completed design on about me

--- client/src/Components/Banner/Banner.js
+++ client/src/Components/Banner/Banner.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './Banner.css'
 
-function Banner({children, title, subtitle,position }) {
+import { Paper, Grid, makeStyles, Typography, Box, Container } from '@material-ui/core';
+//Import profile image
+import portfolio from "../Images/pradeepPhoto.jpg"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: "#f3dfd4"
+    },
+    img: {
+        width: "auto",
+        height: "400px",
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+        borderRadius: "100px",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+}))
+
+
+
+function Banner({ children, title, subtitle, position }) {
+    const classes = useStyles();
     return (
         <div className="banner">
-            <h1>{title}</h1>
-            <h2>{position}</h2>
-            <div></div>
-            <p>{subtitle}</p>
-            {children}
-
+            <Grid item container >
+                <Grid item sm={12} md={6}>
+                    <img alt="complex" className={classes.img} src={portfolio} />
+                    <h1>{title}</h1>
+                    <h2>{position}</h2>
+                </Grid>
+                <Grid item sm={12} md={6}>
+                    <p>{subtitle}</p>
+                    {children}
+                </Grid>
+            </Grid>
         </div>
     )
 }
--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -19,6 +19,17 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
         backgroundColor: "#f3dfd4"
     },
+    img: {
+        width: 130,
+        height: 200,
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+        borderRadius: "100px",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
 }))
 export default function Home() {
     const classes = useStyles();
@@ -27,8 +38,8 @@ export default function Home() {
             <Navbar />
             <Hero>     
 
-                    <Banner title="Pradeep Marasini" position="React Developer" subtitle="Full Stack Web Developer with a background in electronics and communication engineering, utilising experience in banking / payment application, embedded system and agile methodology. Awarded as “Best IT Supporter” and is a strong communicator to work effectively with end-users and the team. Motivated to implement innovative technology on real world problems, to make life easier. Demonstrated excellent people-oriented service and know how to handle difficult requests while prioritising work.">
-                    <img className={classes.img} alt="complex" src={portfolio} />
+                    <Banner title="Pradeep Marasini" position="React Developer" subtitle="Creative React Developer with certificate in Full Stack Web Development from University of Adelaide.Background in electronics and communication engineering, utilising experience in banking / payment application, embedded system and agile methodology. Awarded as “Best IT Supporter” and is a strong communicator to work effectively with end-users and the team. Motivated to implement innovative technology on real world problems, to make life easier. Demonstrated excellent people-oriented service and know how to handle difficult requests while prioritising work.">
+                   
 
                     </Banner>
             </Hero>
